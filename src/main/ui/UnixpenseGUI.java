a26d30252a25a96e340f1d8c032766bec6d09433
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2021-11-17 00:46:37 -0800

    Phase 3 added Panels.

--- src/main/ui/UnixpenseGUI.java
+++ src/main/ui/UnixpenseGUI.java
@@ -9,52 +9,40 @@ public class UnixpenseGUI extends JFrame {
     JPanel tablePanel;
     JPanel buttonsPanel;
 
-    JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(0, 150, 800, 150);
-
-    JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0, 300, 800, 150);
-
     UnixpenseGUI() {
         super("Unixpense");
 
-        ImageIcon image = new ImageIcon("./data/expenses.png");
+//        ImageIcon image = new ImageIcon("./data/expenses.png");
 
         JLabel label = new JLabel();
-        label.setText("Sort by:");
-        label.setIcon(image);
-//        label.setHorizontalTextPosition(JLabel.LEFT);
-//        label.setVerticalTextPosition(JLabel.TOP);
+        label.setText("Unixpense");
+        label.setHorizontalTextPosition(JLabel.LEFT);
+        label.setVerticalTextPosition(JLabel.TOP);
 
-        sortPanel = new JPanel();
-        sortPanel.setBounds(0, 0, 800, 150);
-
-        tablePanel = new JPanel();
-        tablePanel.setBounds(0, 150, 800, 150);
-
-        buttonsPanel = new JPanel();
-        tablePanel.setBounds(0, 300, 800, 150);
-
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(0, 150, 800, 150);
-
-        JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0, 300, 800, 150);
+        setPanel();
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
+        this.setLayout(new GridLayout());
         this.setSize(800,600);
         this.setResizable(false);
         this.setVisible(true);
 
-        redPanel.add(label);
-        this.add(redPanel);
-        this.add(bluePanel);
-        this.add(greenPanel);
+        sortPanel.add(label);
+        this.add(sortPanel);
+        this.add(tablePanel);
+        this.add(buttonsPanel);
 
+    }
+
+    public void setPanel() {
+        sortPanel = new JPanel();
+        sortPanel.setBackground(Color.red);
+        sortPanel.setBounds(0, 0, 800, 200);
+
+        tablePanel = new JPanel();
+        tablePanel.setBounds(0, 200, 800, 200);
+
+        buttonsPanel = new JPanel();
+        tablePanel.setBounds(0, 400, 800, 200);
     }
 }
