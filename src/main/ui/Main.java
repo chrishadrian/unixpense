fe80d98437145021a0765ff630cb6004191277da
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2021-11-19 00:29:38 -0800

    Modify table so it sorted the data based on date

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class Main {
             JFrame frame = new MainFrame("Hello World Swing!");
             frame.setSize(600,400);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
     }
--- src/main/ui/MainFrame.java
+++ src/main/ui/MainFrame.java
@@ -6,7 +6,6 @@ import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
-import javax.swing.event.EventListenerList;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
@@ -16,18 +15,19 @@ import java.time.LocalDate;
 
 public class MainFrame extends JFrame {
 
-    private ImagePanel imagePanel;
-    private TablePanel tablePanel;
-    private ButtonsPanel buttonsPanel;
+    private final ImagePanel imagePanel;
+    private final TablePanel tablePanel;
+    private final ButtonsPanel buttonsPanel;
 
     private Expenses exp;
-    private Expenses tempExp;
     private DefaultTableModel model;
 
     private static final String JSON_STORE = "./data/expenses.json";
 
     MainFrame(String title) {
         super(title);
+
+        new LoadWindow();
 
         // Initialize expenses
         exp = new Expenses();
@@ -53,7 +53,7 @@ public class MainFrame extends JFrame {
         JsonReader jsonReader = new JsonReader(JSON_STORE);
         try {
             exp = jsonReader.read();
-            tablePanel.printExpenses();
+            tablePanel.updateExpenses();
             System.out.println("Loaded Expenses from " + JSON_STORE);
         } catch (IOException e) {
             System.out.println("Unable to read from file: " + JSON_STORE);
@@ -72,7 +72,55 @@ public class MainFrame extends JFrame {
         }
     }
 
-    private class ImagePanel extends JPanel {
+    private class LoadWindow implements ActionListener {
+
+        private final JButton yesBtn;
+        private final JButton noBtn;
+        private JFrame frame;
+
+        public LoadWindow() {
+            setFrame();
+
+            JLabel askLoadLabel = new JLabel("Do you want to load previous expenses?");
+            askLoadLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+            yesBtn = new JButton("Yes");
+            yesBtn.addActionListener(this);
+            noBtn = new JButton("No");
+            noBtn.addActionListener(this);
+            JPanel yesNoPanel = new JPanel(new FlowLayout());
+            yesNoPanel.add(yesBtn);
+            yesNoPanel.add(noBtn);
+
+            Container c = frame.getContentPane();
+            c.add(askLoadLabel, BorderLayout.CENTER);
+            c.add(yesNoPanel, BorderLayout.SOUTH);
+        }
+
+        private void setFrame() {
+            frame = new JFrame();
+            frame.setSize(300,100);
+            frame.setLayout(new BorderLayout());
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.setAlwaysOnTop(true);
+            frame.toFront();
+            frame.repaint();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == yesBtn) {
+                loadExpenses();
+                frame.dispose();
+            } else if (e.getSource() == noBtn) {
+                frame.dispose();
+            }
+        }
+    }
+
+    private static class ImagePanel extends JPanel {
         ImageIcon image;
         JLabel titleLabel;
         JLabel iconLabel;
@@ -114,21 +162,11 @@ public class MainFrame extends JFrame {
             model.setColumnIdentifiers(columnNames);
         }
 
-        private void printExpenses() {
+        private void updateExpenses() {
+            model.setRowCount(0);
+            exp.sortExpensesDate();
             for (int i = 0; i < exp.length(); i++) {
                 Expense ex = exp.getExpense(i);
-                Object[] o = new Object[4];
-                o[0] = ex.getDate();
-                o[1] = ex.getCategory();
-                o[2] = ex.getAmount();
-                o[3] = ex.getComment();
-                model.addRow(o);
-            }
-        }
-
-        private void updateExpenses() {
-            for (int i = 0; i < tempExp.length(); i++) {
-                Expense ex = tempExp.getExpense(i);
                 Object[] o = new Object[4];
                 o[0] = ex.getDate();
                 o[1] = ex.getCategory();
@@ -159,10 +197,9 @@ public class MainFrame extends JFrame {
 
     private class ButtonsPanel extends JPanel implements ActionListener {
 
-        private JButton createBtn;
-        private JButton deleteBtn;
-        private JButton loadBtn;
-        private JButton saveBtn;
+        private final JButton createBtn;
+        private final JButton deleteBtn;
+        private final JButton saveBtn;
 
         public ButtonsPanel() {
             Dimension size = getPreferredSize();
@@ -172,19 +209,16 @@ public class MainFrame extends JFrame {
 
             createBtn = new JButton("Create");
             deleteBtn = new JButton("Delete");
-            loadBtn = new JButton("Load");
             saveBtn = new JButton("Save");
 
             createBtn.addActionListener(this);
             deleteBtn.addActionListener(this);
-            loadBtn.addActionListener(this);
             saveBtn.addActionListener(this);
 
             setLayout(new FlowLayout(FlowLayout.CENTER, 10, 0));
 
             add(createBtn);
             add(deleteBtn);
-            add(loadBtn);
             add(saveBtn);
         }
 
@@ -194,8 +228,6 @@ public class MainFrame extends JFrame {
                 new DateWindow();
             } else if (e.getSource() == deleteBtn) {
                 tablePanel.deleteSelectedRow();
-            } else if (e.getSource() == loadBtn) {
-                loadExpenses();
             } else if (e.getSource() == saveBtn) {
                 saveExpenses();
             }
@@ -206,14 +238,13 @@ public class MainFrame extends JFrame {
 
         private JFrame frame;
 
-        private JLabel askDateLabel;
-        private JButton yesBtn;
-        private JButton noBtn;
+        private final JButton yesBtn;
+        private final JButton noBtn;
 
         public DateWindow() {
             setFrame();
 
-            askDateLabel = new JLabel("Do you want to use current date?");
+            JLabel askDateLabel = new JLabel("Do you want to use current date?");
             askDateLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
             yesBtn = new JButton("Yes");
@@ -225,7 +256,7 @@ public class MainFrame extends JFrame {
             yesNoPanel.add(noBtn);
 
             Container c = frame.getContentPane();
-            c.add(askDateLabel, BorderLayout.NORTH);
+            c.add(askDateLabel, BorderLayout.CENTER);
             c.add(yesNoPanel, BorderLayout.SOUTH);
         }
 
@@ -236,7 +267,6 @@ public class MainFrame extends JFrame {
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frame.setVisible(true);
         }
-
 
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -267,12 +297,10 @@ public class MainFrame extends JFrame {
         private JButton addBtn;
         private JButton resetBtn;
 
-        private Boolean currentDate;
+        private final Boolean currentDate;
 
         public CreateWindow(Boolean currentDate) {
             this.currentDate = currentDate;
-            tempExp = new Expenses();
-
             setFrame();
 
             setLabel();
@@ -331,6 +359,18 @@ public class MainFrame extends JFrame {
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         }
 
+        private void exportExpense() {
+            String temp = dateTF.getText();
+            int year = Integer.parseInt(temp.substring(0, 4));
+            int mon = Integer.parseInt(temp.substring(5, 7));
+            int day = Integer.parseInt(temp.substring(8, 10));
+            LocalDate date = LocalDate.of(year, mon, day);
+
+            Expense ex = new Expense(date, categoryTF.getText(),
+                    Double.parseDouble(amountTF.getText()), commentsTF.getText());
+            exp.addExpense(ex);
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == addBtn) {
@@ -345,20 +385,5 @@ public class MainFrame extends JFrame {
                 commentsTF.setText("");
             }
         }
-
-        private void exportExpense() {
-            String temp = dateTF.getText();
-            int year = Integer.parseInt(temp.substring(0, 4));
-            int mon = Integer.parseInt(temp.substring(5, 7));
-            int day = Integer.parseInt(temp.substring(8, 10));
-            LocalDate date = LocalDate.of(year, mon, day);
-
-            Expense ex = new Expense(date, categoryTF.getText(),
-                    Double.parseDouble(amountTF.getText()), commentsTF.getText());
-            tempExp.addExpense(ex);
-            exp.addExpense(ex);
-        }
     }
-
-
 }
